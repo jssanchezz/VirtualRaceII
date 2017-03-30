53afec0f199682ca20cb416a9da42d674f07bc03
alumno <alumno>
2017-03-30 18:55:45 -0300

    Creacion de clases

---
+++ VirtualRace/VirtualRace/VirtualRace/Auto.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualRace
+{
+    public class Auto
+    {
+        #region Atributos
+
+        public eFabricante fabricante;
+        public Rueda DD;
+        public Rueda DI;
+        public Rueda TD;
+        public Rueda TI;
+
+        public static int contadorDeObjetos;
+        private static Random miNumeroRandom;
+
+        #endregion
+
+        #region Constructores
+
+        public Auto()
+        {
+            
+            this.DD = new Rueda();
+            this.DI = new Rueda();
+            this.TD = new Rueda();
+            this.TI = new Rueda();
+
+            fabricante = (eFabricante)miNumeroRandom.Next(0, 3);
+
+            contadorDeObjetos++;
+        }
+
+        static Auto()
+        {
+            contadorDeObjetos = 0;
+            miNumeroRandom = new Random();
+        }
+
+        #endregion
+    }
+}
---
+++ VirtualRace/VirtualRace/VirtualRace/Rueda.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualRace
+{
+    public class Rueda
+    {
+        #region Atributos
+
+        public string marca;
+        public int tamanio;
+
+        #endregion
+
+        #region Constructores
+
+        public Rueda()
+        {
+            this.marca = "Sin marca.";
+            this.tamanio = 165;
+        }
+
+        #endregion
+
+    }
+}
